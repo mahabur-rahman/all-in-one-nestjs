01127dcc9413f4dc513d8bb5c12c69d45ce0c0b4
mahabur-rahman <annur4395@gmail.com>
2024-06-07 17:40:38 +0600

    slug added with slugify

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -4,6 +4,7 @@ import { Article } from './schemas/article.schema';
 import mongoose from 'mongoose';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { User } from 'src/auth/schemas/user.schema';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticlesService {
@@ -17,9 +18,25 @@ export class ArticlesService {
     createArticleDto: CreateArticleDto,
     user: User,
   ): Promise<Article> {
-    const data = Object.assign(createArticleDto, { author: user._id });
+    const slug = await this.generateUniqueSlug(createArticleDto.title);
+    const data = Object.assign(createArticleDto, { author: user._id, slug });
 
     const newArticle = await this.articleModel.create(data);
     return newArticle;
+  }
+
+  // Generate unique slug
+  private async generateUniqueSlug(title: string): Promise<string> {
+    const slug = slugify(title, { lower: true });
+    let uniqueSlug = slug;
+    let count = 1;
+
+    // Check if the slug already exists in the database
+    while (await this.articleModel.exists({ slug: uniqueSlug })) {
+      uniqueSlug = `${slug}-${count}`;
+      count++;
+    }
+
+    return uniqueSlug;
   }
 }
