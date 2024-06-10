3030b9acfbdcce5646d124747f3a6136329da0af
mahabur-rahman <annur4395@gmail.com>
2024-06-10 19:16:23 +0600

    updated profile

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProfileService } from './profile.service';
+import { User } from 'src/auth/schemas/user.schema';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
+
+  // get Profile
+  @Get(':username')
+  async getProfileByUsername(
+    @Param('username')
+    username: string,
+  ): Promise<User> {
+    return this.profileService.getProfileByUsername(username);
+  }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/auth/schemas/user.schema';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async getProfileByUsername(username: string): Promise<User> {
+    const user = await this.userModel.findOne({ userName: username }).exec();
+    if (!user) {
+      throw new NotFoundException(`User with username '${username}' not found`);
+    }
+    return user;
+  }
+}
