2f7badc90923e1f83dd0d185fdead48f39fbbee4
mahabur-rahman <annur4395@gmail.com>
2024-06-10 23:14:04 +0600

    added profile follow an user and unfollow an user

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { User } from 'src/auth/schemas/user.schema';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('profile')
 export class ProfileController {
@@ -14,5 +23,29 @@ export class ProfileController {
   ): Promise<User> {
     console.log(username);
     return this.profileService.getProfileByUsername(username);
+  }
+
+  // follow user
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard())
+  async followUser(
+    @Param('username') username: string,
+    @Req() req: any,
+  ): Promise<void> {
+    const currentUsername = (req.user as any).userName;
+    await this.profileService.followUser(currentUsername, username);
+  }
+
+  // unfollow user
+  @Delete(':username/unfollow')
+  @UseGuards(AuthGuard())
+  async unfollowUser(
+    @Param('username') username: string,
+    @Req() req: any,
+  ): Promise<void> {
+    const currentUsername = (req.user as any).userName; // Assuming user data is stored in 'userName' property
+
+    await this.profileService.unfollowUser(currentUsername, username);
   }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -16,4 +16,59 @@ export class ProfileService {
     }
     return user;
   }
+
+  // follow user
+
+  async followUser(
+    currentUsername: string,
+    targetUsername: string,
+  ): Promise<void> {
+    const currentUser = await this.userModel
+      .findOne({ userName: currentUsername })
+      .exec();
+
+    const targetUser = await this.userModel
+      .findOne({ userName: targetUsername })
+      .exec();
+
+    if (!targetUser) {
+      throw new NotFoundException(
+        `User with username '${targetUsername}' not found`,
+      );
+    }
+
+    if (currentUser.following.includes(targetUser._id)) {
+      throw new Error(`You are already following '${targetUsername}'`);
+    }
+
+    currentUser.following.push(targetUser._id);
+
+    await currentUser.save();
+  }
+
+  // unfollow user
+  async unfollowUser(
+    currentUsername: string,
+    targetUsername: string,
+  ): Promise<void> {
+    const currentUser = await this.userModel
+      .findOne({ userName: currentUsername })
+      .exec();
+    const targetUser = await this.userModel
+      .findOne({ userName: targetUsername })
+      .exec();
+
+    if (!targetUser) {
+      throw new NotFoundException(
+        `User with username '${targetUsername}' not found`,
+      );
+    }
+
+    // Remove target user ID from following array
+    currentUser.following = currentUser.following.filter(
+      (userId) => !userId.equals(targetUser._id),
+    );
+
+    await currentUser.save();
+  }
 }
