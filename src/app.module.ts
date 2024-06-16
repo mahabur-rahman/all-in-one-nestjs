5efb31675c0f1340f55000e8e26c48838ab221fe
mahabur-rahman <annur4395@gmail.com>
2024-06-16 19:48:39 +0600

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
-import { LessonModule } from './lesson/lesson.module';
-import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
@@ -22,10 +20,6 @@ import { StudentModule } from './student/student.module';
       autoSchemaFile: true,
       playground: true,
     }),
-
-    LessonModule,
-
-    StudentModule,
   ],
   providers: [AppResolver, AppService],
 })
