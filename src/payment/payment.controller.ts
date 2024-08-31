b1b1a2432ba166f2032c528ce63708b4ea8a8a22
mahabur-rahman <annur4395@gmail.com>
2024-08-31 21:16:35 +0600

    fix: redirect to the client

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Param, Post, Res } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { Response } from 'express';
 
 @Controller()
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post('payment/success/:transactionId')
-  async getSuccessPayment(@Param('transactionId') transactionId: string) {
+  async getSuccessPayment(
+    @Param('transactionId') transactionId: string,
+    @Res() res: Response,
+  ) {
     console.log('Payment success for transaction:', transactionId);
 
-    return await this.paymentService.successPayment(transactionId);
+    await this.paymentService.successPayment(transactionId);
+
+    // Redirect to the frontend URL
+    res.redirect(`http://localhost:3000/payment/success/${transactionId}`);
   }
 }
