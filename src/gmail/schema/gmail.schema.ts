5babb910bbb44f5c2b3334be70adf65861561d0a
mahabur-rahman <annur4395@gmail.com>
2024-09-27 21:42:44 +0600

    feat: Schema declare for gmail

---
+++ src/gmail/schema/gmail.schema.ts
@@ -0,0 +1,30 @@
+import { registerEnumType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export enum GmailStatus {
+  PENDING = 'PENDING',
+  VERIFIED = 'VERIFIED',
+}
+
+// register enum for graphql
+registerEnumType(GmailStatus, {
+  name: 'GmailStatus',
+});
+
+@Schema({
+  timestamps: true,
+})
+export class Gmail extends Document {
+  @Prop({
+    unique: [true, `Duplicate email entered!`],
+  })
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ enum: GmailStatus, default: GmailStatus.PENDING })
+  status: GmailStatus;
+}
+
+export const GmailSchema = SchemaFactory.createForClass(Gmail);
