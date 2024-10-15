515fdfe9f343e4709f6e8f480c9376dab90664b0
mahabur-rahman <annur4395@gmail.com>
2024-10-16 00:00:57 +0600

    feat: declare grapql type

---
+++ src/calender/types/calender.type.ts
@@ -0,0 +1,28 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class CalendarType {
+  @Field(() => ID)
+  _id?: string;
+
+  @Field()
+  title: string;
+
+  @Field()
+  startDate: string;
+
+  @Field({ nullable: true })
+  endDate?: string;
+
+  @Field()
+  allDay: boolean;
+
+  @Field({ nullable: true })
+  url?: string;
+
+  @Field({ nullable: true })
+  backgroundColor?: string;
+
+  @Field({ nullable: true })
+  borderColor?: string;
+}
