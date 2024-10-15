2364604f7e848a0d3dbe3a1e965dd69cb7865806
mahabur-rahman <annur4395@gmail.com>
2024-10-15 22:44:58 +0600

    feat: Event schema define

---
+++ src/calender/schema/calender.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CalendarDocument = Calendar & Document;
+
+@Schema({
+  timestamps: true,
+})
+export class Calendar {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  startDate: Date;
+
+  @Prop()
+  endDate: Date;
+
+  @Prop({ default: false })
+  allDay: boolean;
+
+  @Prop({ required: false })
+  url?: string;
+
+  @Prop({ required: false })
+  backgroundColor?: string;
+
+  @Prop({ required: false })
+  borderColor?: string;
+}
+
+export const CalendarSchema = SchemaFactory.createForClass(Calendar);
