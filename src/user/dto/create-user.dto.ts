f4c85f39f1ff3e8f1450b59a47c575e2c50db337
mahabur-rahman <annur4395@gmail.com>
2024-10-21 13:49:54 +0600

    feat: crated user logic added for postgres

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -6,27 +6,34 @@ import {
   IsString,
   MinLength,
 } from 'class-validator';
-import { UserRole } from '../entities/user.entity';
+import { InputType, Field } from '@nestjs/graphql'; // Import InputType and Field
+import { UserRole } from '../entities/user.entity'; // Import UserRole enum
 
+@InputType() // Add this decorator to mark the class as a GraphQL input type
 export class CreateUserDto {
+  @Field() // Expose field to GraphQL
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @Field() // Expose field to GraphQL
   @IsString()
   @IsNotEmpty()
   lastName: string;
 
+  @Field() // Expose field to GraphQL
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @Field() // Expose field to GraphQL
   @IsString()
   @MinLength(6)
   @IsNotEmpty()
   password: string;
 
+  @Field(() => UserRole, { nullable: true }) // Expose field to GraphQL
   @IsOptional()
   @IsEnum(UserRole)
-  role?: UserRole; // Optional, defaults to UserRole.USER
+  role?: UserRole; // Optional field
 }
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserType } from './types/user.type';
 
-@Resolver()
+@Resolver(() => UserType)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserType)
+  async createUser(
+    @Args('createUserInput') createUserDto: CreateUserDto,
+  ): Promise<UserType> {
+    return this.userService.createUser(createUserDto);
+  }
 }
