c1bffd3322177fbd1f0b57cf33e494ae0833f3a7
mahabur-rahman <annur4395@gmail.com>
2024-10-21 11:59:07 +0600

    remove uuid

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Task {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
   id: string;
 
   @Column()
