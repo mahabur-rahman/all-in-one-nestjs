eba9a86d53117017a82272ecc2a5e7cf52fc7a61
mahabur-rahman <annur4395@gmail.com>
2024-12-07 15:50:00 +0600

    feat: Cloud add

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,9 +1,20 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+// export const dataSourceOptions: DataSourceOptions = {
+//   type: 'mongodb',
+//   host: 'localhost',
+//   port: 27017,
+//   database: 'db-migrations',
+//   entities: ['dist/**/*.entity.js'],
+//   migrations: ['dist/db/migrations/*.js'],
+//   synchronize: true,
+// };
+
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mongodb',
-  host: 'localhost',
-  port: 27017,
+  url:
+    process.env.MONGO_URI ||
+    'mongodb+srv://mahabur:mahabur@cluster0.2b3niye.mongodb.net/db-migrations',
   database: 'db-migrations',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
